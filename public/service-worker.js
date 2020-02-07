74e1f04de19be3f8c05ee2eca34857f6313a5c8c
Glitch (holistic-syzygy) <none>
2020-02-07 02:14:31 +0000

    ⛑🚝 Updated with Glitch

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,8 +18,8 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME      = 'static-cache-v49';
-const DATA_CACHE_NAME = 'data-cache-v49';
+const CACHE_NAME      = 'static-cache-v50';
+const DATA_CACHE_NAME = 'data-cache-v50';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
